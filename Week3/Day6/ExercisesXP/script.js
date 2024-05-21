ea5922419a7c1f1f5fbc2eed9d17df686f38c724
Koltanuk <pavl.ira.a@gmail.com>
2024-05-21 16:45:22 +0300

    ExerciseXP w3d6 till end

--- Week3/Day6/ExercisesXP/script.js
+++ Week3/Day6/ExercisesXP/script.js
@@ -29,3 +29,95 @@ for (let i = 0; i < people.length; i++) {
     console.log(people[i]);
     if(people[i]=="Devon"){break;}
 }
+
+// Exercise 2
+
+let colors = ["red", "blue", "yellow", "green", "purple"];
+for (let i = 0; i < colors.length; i++) {
+    console.log("My #" + (i+1) + " choice is "+colors[i]);
+}
+//bonus
+let suffixes = ["st", "nd", 'rd', "th"];
+
+for (let i = 0; i < colors.length; i++) {
+    if(i<3){
+        console.log("My "+ (i+1) + suffixes[i]+ " choice is "+colors[i]);
+    }
+    else{
+        console.log("My "+ (i+1) + suffixes[3]+ " choice is "+colors[i]);
+    }
+}
+
+//Exercise 3
+let num = parseInt(prompt("please enter a number"));
+while(num<10){
+    num = parseInt(prompt("please enter a number"));
+}
+
+//Exercise 4
+
+const building = {
+    numberOfFloors: 4,
+    numberOfAptByFloor: {
+        firstFloor: 3,
+        secondFloor: 4,
+        thirdFloor: 9,
+        fourthFloor: 2,
+    },
+    nameOfTenants: ["Sarah", "Dan", "David"],
+    numberOfRoomsAndRent:  {
+        sarah: [3, 990],
+        dan:  [4, 1000],
+        david: [1, 500],
+    },
+}
+
+console.log(building.numberOfFloors);
+console.log(building.numberOfAptByFloor.firstFloor);
+console.log(building.numberOfAptByFloor.thirdFloor);
+console.log(building.nameOfTenants[1]+", "+building.numberOfRoomsAndRent.dan[0]);
+if(building.numberOfRoomsAndRent.david[1]+building.numberOfRoomsAndRent.sarah[1] > building.numberOfRoomsAndRent.dan[1] ){
+    building.numberOfRoomsAndRent.dan[1] = 1200;
+}
+console.log(building.numberOfRoomsAndRent.dan[1]);
+
+//Exercise 5
+
+let family = {
+    father: "John",
+    mother: "Jane",
+    son: "Mike",
+    daughter: "Mary"
+};
+
+
+for (let key in family) {
+    console.log(key);
+}
+
+for (let key in family) {
+    console.log(family[key]);
+}
+
+//Exercise 6
+
+const details = {
+    my: 'name',
+    is: 'Rudolf',
+    the: 'raindeer'
+  }
+
+  for (let key in details) {
+    console.log(key + " "+details[key]);
+}
+
+//Exercise 7
+const names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"];
+let alfNames = names.sort();
+
+let result="";
+for (let i = 0; i < alfNames.length; i++) {
+    result+=alfNames[i].charAt(0);
+}
+console.log(result);
+
